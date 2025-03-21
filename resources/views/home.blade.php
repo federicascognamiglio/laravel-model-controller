c667dac4195e2e2da9a75d74a6751f19fc380fe5
federicascognamiglio <scognamigliofede99@gmail.com>
2025-03-21 16:22:13 +0100

    feat: movie cards printed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,17 @@
-@extends('Layouts.master')
+@extends('Layouts.master')  
 
 @section('content')
 <div class="container">
-    <h1>home</h1>
+    <h1 class="text-center my-5">LARAVEL MOVIES</h1>
+
+    <!-- Movie Cards -->
+    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4">
+        @foreach($movies as $movie)
+        <div class="col mb-4">
+            <x-card :movie="$movie" />
+        </div>
+        @endforeach
+    </div>
+    <!-- /Movie Cards -->
 </div>
 @endsection
